9334f7e3f00fc1200d758fe5bf6cd47bed288811
Gal Bahar <info@monster-studio.net>
2022-05-10 18:07:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,28 +6,6 @@ const app = express();
 
 app.use(bodyParser.json())
 
-var object = {
-    "config": {
-      "id": ".id",
-      "email": ".details.email",
-      "full_name": ".user_details.name + payload.user_details.last_name"
-    },
-    "payload": {
-      "id": "123",
-      "user_details": {
-        "name": "Sam",
-        "last_name": "One",
-        "id": "111"
-      },
-      "details": {
-        "email": "1@1.com"
-      }
-    }
-  };
-
-  const test = _.get(object, object.config.id);
-
-
 app.listen( 3000, () => console.log(test))
 
 app.post('/api', (req, res) => {
